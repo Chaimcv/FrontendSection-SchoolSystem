88e344cd7cb8515daac49a21cadff3963d3d6a10
Chaimcv <chaithanyamcv@gmail.com>
2026-02-14 16:59:00 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
-import {BrowserRouter, Routes} from "react-router-dom";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import AdminRoutes from './Routes/AdminRoutes';
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
+         <Header />
       <BrowserRouter>
       <Routes>
+     
         <Route path="/admin/*" element={<AdminRoutes />}/>
+     
       </Routes>
       </BrowserRouter>
+         <Footer />
     </div>
   );
 }
--- src/pages/AdminLogin.jsx
+++ src/pages/AdminLogin.jsx
@@ -1,14 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const AdminLogin = () => {
+    const navigate =useNavigate();
+    const[adminEmail,setAdminemail]=useState();
+    const[adminPassword,setAdminPassword]=useState();
+  const adminEmailSaved=process.env.REACT_APP_API_ADMIN_EMAILS.split(',');
+  const adminPasswordSaved=process.env.REACT_APP_API_ADMIN_PASSWORDS.split(',');
+  console.log(adminEmailSaved,"admin emails");
+   console.log(adminEmail);
+   console.log(adminPassword);
+   const checkValidAdmin=()=>{
+     if(adminEmailSaved.includes(adminEmail)&& adminPasswordSaved.includes(adminPassword)){
+        alert("Login Successfull");
+        navigate("/dashboard");
+     }
+    else{
+        alert("Invalid Credentials");
+    }
+   } 
   return (
-    <div>AdminLogin
+    <div className='bg-red-400'>
         <div>
-            <h3>email </h3><input type='email'></input>
-             <h3>Password </h3><input type='password'></input>
-             <button>Login</button>
+            <h3>email </h3><input type='email' value={adminEmail} onChange={(e)=>setAdminemail(e.target.value)}></input>
+             <h3>Password </h3><input type='password' value={adminPassword} onChange={(e)=>setAdminPassword(e.target.value)}></input>
+             <button onClick={checkValidAdmin()}>Login</button>
         </div>
-        <div><h5>New Admin? Register here</h5><button>SignUp</button></div>
+        {/* <div><h5>New Admin? Register here</h5><button onClick={navigateToRegistration()}>SignUp</button></div> */}
     </div>
   )
 }
