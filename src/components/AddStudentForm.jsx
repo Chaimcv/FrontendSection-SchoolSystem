c8b2992a0c4f301f4c615f53eb50d2dd97d65915
Chaimcv <chaithanyamcv@gmail.com>
2026-02-18 19:37:42 +0530

    updates

--- src/components/AddStudentForm.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const AddStudentForm = () => {
-  return (
-    <div className='bg-yellow-200'>AddStudentForm</div>
-  )
-}
-
-export default AddStudentForm
--- src/pages/views/Student/AddNewStudent.jsx
+++ src/pages/views/Student/AddNewStudent.jsx
@@ -2,7 +2,22 @@ import React from 'react'
 
 const AddNewStudent = () => {
   return (
-    <div>AddNewStudent</div>
+    <div>
+        <div></div>
+        <div>
+            <input type='text'>NAME</input>
+             <input type='text'>AGE</input>
+              <input type='text'>GENDER</input>
+               <input type='text'>STANDARD</input>
+                <input type='text'>DIVISION</input>
+                 <input type='text'>GUARDIAN NAME</input>
+                  <input type='text'>GUARDIAN PHONENUMBER</input>
+                   <input type='text'>ADDRESS</input>
+                    <input type='text'>PINCODE</input>
+                    <button>SUBMIT</button>
+        </div>
+        <div></div>
+    </div>
   )
 }
 
