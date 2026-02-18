35178c197b673c6b5724d427c84105b3519a9be0
Chaimcv <chaithanyamcv@gmail.com>
2026-02-18 21:03:44 +0530

    updates

--- src/pages/views/Student/AddNewStudent.jsx
+++ src/pages/views/Student/AddNewStudent.jsx
@@ -1,19 +1,26 @@
 import React, { useState } from 'react'
+import { useDispatch } from 'react-redux';
+import { addNewStudentToDb } from '../../../Redux/Slices/StudentSlice';
+import { useNavigate } from 'react-router-dom';
 
 const AddNewStudent = () => {
-    const[data,setformData]=useState();
+    const[formdata,setformData]=useState();
+    const dispatch=useDispatch();
+    const navigate=useNavigate();
+    
     const handleChange=(e)=>{
         setformData(e.target.value);
     }
     const AddStudent=()=>{
-        
+    dispatch(addNewStudentToDb(formdata));
+    navigate("/allStudentslisted");
     }
   return (
     <div className='bg-amber-200 border border-amber-600 '>
-    
-   
-          <div className='h-screen flex'>
-            <form onSubmit={AddStudent}>
+    <h2 className='align-middle font-semibold'>Add Student Details</h2>
+   <form onSubmit={AddStudent}>
+          <div className='h-full flex flex-col m-5 p-5 space-y-5'>
+            
             <label>NAME<input className='' type='text' onChange={handleChange} /></label>
             <label>AGE<input type='text'  onChange={handleChange} /></label>
              <label>GENDER<input type='text' onChange={handleChange} /></label>
@@ -24,9 +31,10 @@ const AddNewStudent = () => {
                  <label>ADDRESS<input type='text'  onChange={handleChange} /></label>
                     <label>PINCODE<input type='text' onChange={handleChange} /></label>
                     
-              <button>SUBMIT</button>
-             </form>     
+              <button className='bg-amber-400  '>SUBMIT</button>
+              
        </div>
+         </form>  
         </div>
        
 
