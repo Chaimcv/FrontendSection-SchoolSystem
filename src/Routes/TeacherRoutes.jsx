af2bad7fed215fb10833a22c87aaccf7d70048ed
Chaimcv <chaithanyamcv@gmail.com>
2026-02-18 10:43:57 +0530

    additions

--- src/Routes/TeacherRoutes.jsx
+++ src/Routes/TeacherRoutes.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import TeachersDashboard from '../pages/TeachersDashboard'
 import ViewTeacherDetails from '../pages/ViewTeacherDetails'
+import TeacherLogin from '../pages/views/Teacher/TeacherLogin'
 
 const TeacherRoutes = () => {
  
@@ -10,6 +11,7 @@ const TeacherRoutes = () => {
   return (
     <div className='bg-gray-300'>
         <Routes>
+           <Route path="/teacherlogin" element={<TeacherLogin />}/>
             <Route path="/teacherDashboard" element={<TeachersDashboard />}/>
              <Route path="/:teacherid" element={<ViewTeacherDetails />}/>
         </Routes>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,6 +5,7 @@ import { fetchTeachersData } from '../Redux/Slices/TeacherSlice';
 const Login = () => {
     const[inputtedEmail,setInputtedEmail]=useState();
     const[inputtedPassword,setInputtedPassword]=useState();
+    const navigate=useNavigate();
 
     const DatabaseValues=useSelector((state)=>state.teacher.teacherData);
     console.log(DatabaseValues,"valuesss");
@@ -16,9 +17,18 @@ const Login = () => {
      console.log(inputtedEmail,"email");
      console.log(inputtedPassword,"password");
     }
-   
+   const TeacherLoginPage=()=>{
+    navigate("/teacherlogin");
+   }
+    const PrincipalLoginPage=()=>{
+    navigate("/adminlogin");
+   }
   return (
     <div>
+      <div>
+        <button onClick={TeacherLoginPage}>LOGIN AS TEACHER</button>
+         <button onClick={PrincipalLoginPage}>LOGIN AS PRINCIPAL</button>
+      </div>
         <div className=' border border-gray-600 rounded-md bg-slate-400 m-10 p-5 space-y-5'>
           <label >Email:  <input className='p-2 m-1'type='text'value={inputtedEmail} onChange={(e)=>setInputtedEmail(e.target.value)} placeholder='Enter your email'></input></label><br/> 
             <label >Password<input className='p-2 m-1' type='password' value={inputtedPassword} onChange={(e)=>setInputtedPassword(e.target.value)} placeholder='Enter your password'></input></label> <br />
