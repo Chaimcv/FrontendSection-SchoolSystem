2c06881f3fbc54e4df64bc3996ee218b5f4c30f6
Chaimcv <chaithanyamcv@gmail.com>
2026-02-19 16:49:38 +0530

    updates

--- src/pages/views/Parent/AddParent.jsx
+++ src/pages/views/Parent/AddParent.jsx
@@ -1,19 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useSelector } from 'react-redux';
 
 const AddParent = () => {
    const students= useSelector((state)=>state.student.studentData);
- const [{ Name }]  =students;
-    console.log(Name);
+   //const[nameSelection,setNameSelection]=useState();
+   const[formData,setFormData]=useState({
+    name:"",
+    email:"",
+    studentname:"",
+    studentId:"",
+    address:"",
+    pincode:""
+
+   });
+    console.log(students);
+
     const AllStudents=()=>{
       
     }
+
+     const handleChange = (e) => {
+  const { name, value } = e.target;
+
+  setFormData((prev) => ({
+    ...prev,
+    [name]: value
+  }));
+};
   return (
-    <div>
+    <div className='bg-green-300 h-screen '>
+       
         <form>
-            <div>
-                {/* <select value={AllStudents}></select>
-                <input type=''></input> */}
+            <div className='flex flex-col m-16 p-4 space-y-4'>
+                  <label>STUDENT NAME: 
+                <select value={formData.studentname} onChange={handleChange}>
+                    {students.map((item)=>(
+            <option key={students.id} value={students.Name} >{item.Name}</option>
+            
+        ))}
+        </select>
+        </label>
+             <label>NAME :   <input type='text' name="name" value={formData.name} onChange={handleChange}></input></label>
+              <label>EMAIL :  <input type='email' name="email" value={formData.email} onChange={handleChange}></input></label>
+               <label>ADDRESS :  <input type='text' name="address" value={formData.address} onChange={handleChange}></input></label>
+                <label>PINCODE :<input type='number' name="pincode" value={formData.pincode} onChange={handleChange}></input></label>
+                {/* <input type='text' name="" onChange={handleChange}></input> */}
+                <button className='bg-green-600 p-2 rounded-lg'>Sumbit</button>
             </div>
         </form>
     </div>
