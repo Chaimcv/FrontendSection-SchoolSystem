6a50afe2e01b23f7ace25b7550fe4c19af26f8ab
Chaimcv <chaithanyamcv@gmail.com>
2026-02-21 12:45:31 +0530

    updates

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
+import HeaderImage from "../assets/HeaderImage.png";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <div
+      className="h-[100px] w-full bg-cover bg-center flex items-center justify-center"
+      style={{ backgroundImage: `url(${HeaderImage})` }}
+    >
+      {/* <div className="bg-black/50 w-full h-full flex items-center justify-center">
+      </div> */}
+    </div>
+  );
+};
 
-export default Header
+export default Header;
